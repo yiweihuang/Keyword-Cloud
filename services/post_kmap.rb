87315fc19240b1a55df9651bb6251dd99211d6c8
yiweihuang <n1993212@gmail.com>
2017-05-09 12:49:55 +0800

    add video and discussiob

--- services/post_kmap.rb
+++ services/post_kmap.rb
@@ -3,11 +3,11 @@ require 'url'
 require 'json'
 
 class PostKmap
-  def self.call(course_id:, chapter_id:, kmap_json:)
+  def self.call(course_id:, chapter_id:, kmap_json:, kmap_video_info:, kmap_discussion_info:)
     kmap_json_string = kmap_json.split("\n")[0]
     parse_json = kmap_json_string.gsub!("'", "\"")
     uri = URI(ENV['Kmap'])
-    res = Net::HTTP.post_form(uri, 'data' => parse_json, 'course_id' => course_id, 'chapter_id' => chapter_id)
+    res = Net::HTTP.post_form(uri, 'data' => parse_json, 'course_id' => course_id, 'chapter_id' => chapter_id, 'kmap_video_info' => kmap_video_info, 'kmap_discussion_info' => kmap_discussion_info)
     JSON.parse(res.body)
   end
 end
